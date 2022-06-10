d3aca2d670afd7b597eb1aa48acadb02a7c48840
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-09 21:14:40 -0300

    feat: Linked list file

---
+++ lista_encadeada.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct t_lista {
+  int chave;
+  struct t_lista * prox;
+} typedef lista;
+
+void imprimir(lista **cabeca){
+  lista *temp;
+  temp = *cabeca;
+  while(temp != NULL) {
+    cout<<temp -> chave<<endl;
+    temp = temp -> prox;
+  }
+}
+
+void inserir(lista **cabeca, int chave) {
+  lista *no = (lista *) malloc (sizeof(lista));
+  no -> chave = chave;
+  no -> prox = NULL;
+  lista *temp = *cabeca;
+  if (temp == NULL) {
+    *cabeca = no;
+  } else {
+    while(temp -> prox != NULL) {
+      temp = temp -> prox;
+    }
+    temp -> prox = no;
+  }
+}
+
+void excluir(lista **cabeca, int chave){
+  lista *temp = *cabeca;
+  lista *ant = NULL;
+  while(temp != NULL){
+    cout<<temp -> chave<<endl;
+    if (temp -> chave == chave) {
+      if (ant == NULL) {
+        *cabeca = temp -> prox;
+      } else {
+        ant -> prox = temp -> prox;
+      }
+      free(temp);
+    }
+    ant = temp;
+    temp = temp -> prox;
+  }
+}
+
+lista * buscar(lista **cabeca, int chave) {
+  lista *temp = *cabeca;
+  while(temp != NULL) {
+    if (temp -> chave == chave) {
+      return temp;
+    }
+    temp = temp -> prox;
+  }
+  return NULL;
+}
+
+int main() {
+  lista *cabeca = NULL;
+  inserir(&cabeca, 10);
+  inserir(&cabeca, 20);
+  inserir(&cabeca, 30);
+
+  imprimir(&cabeca);
+  
+  //excluir(&cabeca, 20);
+  //excluir(&cabeca, 30);
+  //excluir(&cabeca, 10);
+
+  return 0;
+}
